f3b7d458fa6e099d17e854784598a924094eb613
phyzical <5182053+phyzical@users.noreply.github.com>
2022-07-21 13:59:09 +0800

    Add ability to deploy to multiple projects
    * for some projects they are used as a dependecy for multiple other project i.e shib-sp
    
    feature/ability-to-deploy-to-multiple-projects
    
    File List:
    action.yml
    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-project=${1:-$ECR_REPOSITORY}
+projects=${1}
 tag=${2:-$IMAGE_TAG}
 serial_number=${3:-$GITHUB_RUN_NUMBER}
 # optional argument to specify which container in the pod (defaults to first)
@@ -24,39 +24,41 @@ git_password=$(aws ssm get-parameter --name git-ssl-password --query "Parameter.
 
 git clone --depth 1 "https://${git_user}:${git_password}@git-codecommit.ap-southeast-2.amazonaws.com/v1/repos/manifests" "$dir"
 pushd "$dir"
-directories=("applications/${project}/overlays/development/")
-environments="development"
-if [ "$branch" == "master" ] || [ "$branch" == "main" ] ; then
-  environments="production/test"
-  directories=(
-      "applications/${project}/overlays/test/"
-      "applications/${project}/overlays/production/"
-    )
-fi
-
-for directory in "${directories[@]}";
+for project in $(echo $projects | tr "," "\n");
 do
-  serial_number_filename="${directory}/container_${container_index}_serial_number.txt"
-  patch_image_filename="${directory}/patch_image_${container_index}_tag.yaml"
-  # Ensure we don't accidentally overwrite newer images updates
-  if [ -f "$serial_number_filename" ]; then
-    last_serial_number=$(<"$serial_number_filename")
-    if [ "$last_serial_number" -ge "$serial_number" ]; then
-      echo "Attempted to update image with serial number $serial_number, but previous serial number was $last_serial_number. Image not updated."
-      exit 1
-    fi
+  directories=("applications/${project}/overlays/development/")
+  environments="development"
+  if [ "$branch" == "master" ] || [ "$branch" == "main" ] ; then
+    environments="production/test"
+    directories=(
+        "applications/${project}/overlays/test/"
+        "applications/${project}/overlays/production/"
+      )
   fi
 
-  echo "$serial_number" > "$serial_number_filename"
+  for directory in "${directories[@]}";
+  do
+    serial_number_filename="${directory}/container_${container_index}_serial_number.txt"
+    patch_image_filename="${directory}/patch_image_${container_index}_tag.yaml"
+    # Ensure we don't accidentally overwrite newer images updates
+    if [ -f "$serial_number_filename" ]; then
+      last_serial_number=$(<"$serial_number_filename")
+      if [ "$last_serial_number" -ge "$serial_number" ]; then
+        echo "Attempted to update image with serial number $serial_number, but previous serial number was $last_serial_number. Image not updated."
+        exit 1
+      fi
+    fi
 
-  cat <<-EOF > "${patch_image_filename}"
-  - op: replace
-    path: /spec/template/spec/containers/${container_index}/image
-    value: "${oci_repo_url}:$tag"
+    echo "$serial_number" > "$serial_number_filename"
+
+    cat <<-EOF > "${patch_image_filename}"
+    - op: replace
+      path: /spec/template/spec/containers/${container_index}/image
+      value: "${oci_repo_url}:$tag"
 
 EOF
+  done
 done
-
 
 git config user.email "ci@aaf.edu.au"
 git config user.name "AAF CI"
